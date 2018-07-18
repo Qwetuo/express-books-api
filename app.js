b5298edffa4d19d772fbcb318912eee7265c33e6
qwetuo <chewminzhuang@hotmail.com>
2018-07-18 17:39:32 +0800

    commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken")
+const {setTokenInRequest} = require("./middlewares/auth")
 
 const index = require("./routes/index");
 const books = require("./routes/books");
@@ -16,10 +18,21 @@ db.on("error", error => {
 });
 
 const app = express();
+app.use(express.json())
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/secret", setTokenInRequest, (req,res,next) => {
+  jwt.verify(req.token, "some_secret", (err, data) => {
+    if(err) {
+      next(err);
+    } else {
+      res.json({message: "here's your super secret message", data})
+    }
+  })
+})
 
 app.use("/", index);
 app.use("/books", books);
---
+++ middlewares/auth.js
@@ -0,0 +1,16 @@
+const setTokenInRequest = (req, res, next) => {
+    const bearerHeader = req.headers["authorization"];
+    if (bearerHeader) {
+      const bearerToken = bearerHeader.split(" ")[1];
+      req.token = bearerToken;
+  
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  };
+  
+  module.exports = {
+    setTokenInRequest
+  };
+  
